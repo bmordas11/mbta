072b5acd2db0f13f3e5cb27d57e4738eb129cfeb
bmordas11 <brett.mordas@gmail.com>
2016-05-08 16:30:11 -0400

    Fixed many bugs relating to the Stop show-pages

--- app/controllers/commuters_controller.rb
+++ app/controllers/commuters_controller.rb
@@ -1,67 +1,23 @@
-require 'pry'
-require 'time'
-
-class MBTACommunicator
-  def self.get_stop(query, parameter)
-    HTTParty.get(
-      "http://realtime.mbta.com/developer/api/v2/#{query}?" +
-      "api_key=wX9NwuHnZU2ToO7GmGR9uw#{parameter}&format=json"
-    )
-  end
-end
-
 class CommutersController < ApplicationController
   def index
-    @all_routes = ROUTES
+    @all_routes = Commuter::ROUTES
   end
 
   def show
     @desired_route = params[:id]
-    if PROBLEM_ROUTES.include? @desired_route
-      substitue_problem_route @desired_route
+    if Commuter::PROBLEM_ROUTES.include? @desired_route
+      Commuter.substitue_problem_route @desired_route
     end
     @desired_route.prepend('CR-').gsub! ' Line', ''
 
     query = 'stopsbyroute'
     parameter = "&route=#{@desired_route}"
-    the_info = MBTACommunicator.get_stop(query, parameter)
+    the_info = Commuter.get_stop(query, parameter)
     @all_stops = []
-    the_info['direction'][0]['stop'].each do |stop|
-      @all_stops << Stop.find_or_create_by(name: stop['stop_name'])
+    if the_info['direction'].present?
+      the_info['direction'][0]['stop'].each do |stop|
+        @all_stops << Stop.find_or_create_by(name: stop['stop_name'])
+      end
     end
-    @inbound_scheduled_arrival = 'Under construction'
-    @outbound_scheduled_departure = 'Outbound info soon to come'
   end
-
-  def substitue_problem_route(route)
-    route.gsub!('Framingham/', '')
-    route.gsub!('Kingston/', '')
-    route.gsub!('Middleborough/', '')
-    route.gsub!('Newburyport/', '')
-    route.gsub!('Providence/', '')
-  end
-
-  PROBLEM_ROUTES = [
-    'Framingham/Worcester Line',
-    'Kingston/Plymouth Line',
-    'Middleborough/Lakeville Line',
-    'Newburyport/Rockport Line',
-    'Providence/Stoughton Line'
-  ].freeze
-
-  ROUTES = [
-    "Fairmount Line",
-    "Fitchburg Line",
-    "Framingham/Worcester Line",
-    "Franklin Line",
-    "Greenbush Line",
-    "Haverhill Line",
-    "Kingston/Plymouth Line",
-    "Lowell Line",
-    "Middleborough/Lakeville Line",
-    "Needham Line",
-    "Newburyport/Rockport Line",
-    "Providence/Stoughton Line"
-  ].freeze
-
 end
